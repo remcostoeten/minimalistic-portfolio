ed5d3e264dd3b375baa832468c6f90c9da368fd6
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-21 18:32:36 +0200

    add utils

---
+++ lib/experience.ts
@@ -0,0 +1,26 @@
+export const WorkExperience = [
+    {
+        title: 'College - graduated',
+        description:
+            'Graduated as graphic designer.',
+        date: '2013 - 2017',
+    },
+    {
+        title: 'Tickles digital agency',
+        description:
+            'Magento 2 front-end developer.',
+        date: '2017 - 2021',
+    },
+    {
+        title: 'Distil / Lasaulec B.V.',
+        description:
+            'Software (FE) for SaaS and inhouse tooling.',
+        date: '2021 - 2023',
+    },
+    {
+        title: 'Pleio',
+        description:
+            'Frontend for non profit and goverment.',
+        date: '2023 - current',
+    },
+];
