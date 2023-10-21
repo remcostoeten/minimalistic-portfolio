f66ec7682cc321a238d2b95eb97b548982bbfbf4
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-21 22:28:03 +0200

    Add icons component.

---
+++ components/icons/CompanyIcons.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const Icons = [
+    {
+        name: "Gitlab",
+        url: "https://gitlab.com",
+        icon: `<svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='32'
+                    height='32'
+                    fill='none'
+                    viewBox='0 0 32 32'
+                >
+                    <path
+                        fill='#E24329'
+                        d='M31.462 12.779l-.045-.115-4.35-11.35a1.137 1.137 0 00-.447-.541 1.163 1.163 0 00-1.343.071c-.187.15-.322.356-.386.587l-2.94 9.001h-11.9l-2.941-9A1.138 1.138 0 006.065.592a1.153 1.153 0 00-1.13.72L.579 12.68l-.045.113a8.09 8.09 0 002.68 9.34l.016.012.038.03 6.635 4.967 3.28 2.484 1.994 1.51a1.35 1.35 0 001.627 0l1.994-1.51 3.282-2.484 6.673-4.997.018-.013a8.088 8.088 0 002.69-9.352z'
+                    ></path>
+                    <path
+                        fill='#FC6D26'
+                        d='M31.462 12.779l-.045-.115a14.748 14.748 0 00-5.856 2.634l-9.553 7.24A11225.6 11225.6 0 0022.1 27.14l6.673-4.997.019-.013a8.09 8.09 0 002.67-9.352z'
+                    ></path>
+                    <path
+                        fill='#FCA326'
+                        d='M9.908 27.14l3.275 2.485 1.994 1.51a1.35 1.35 0 001.627 0l1.994-1.51 3.282-2.484s-2.835-2.14-6.092-4.603l-6.08 4.603z'
+                    ></path>
+                    <path
+                        fill='#FC6D26'
+                        d='M6.435 15.305A14.712 14.712 0 00.58 12.672l-.045.113a8.09 8.09 0 002.68 9.347l.016.012.038.03 6.635 4.967 6.105-4.603-9.573-7.233z'
+                    ></path>
+                </svg>`,
+    },
+    {
+        name: "GitHub",
+        url: "https://github.com",
+        icon: `<svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='32'
+                    height='32'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke='currentColor'
+                >
+                    <path
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        stroke-width='2'
+                        d='M9 19c-4.418 1.38-4.418-2.62-6-3m12 5v-3.5a2.5 2.5 0 00-2.5-2.5h-2.086a1.5 1.5 0 01-1.414-1.414l-.414-.414a3.001 3.001 0 00-2.121-1.086H7.5a2.5 2.5 0 00-2.5 2.5V21'
+                    ></path>
+                    <path
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        stroke-width='2'
+                        d='M16 3.5a4 4 0 00-8 0 4 4 0 008 0z'
+                    ></path>
+                </svg>`,
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com",
+        icon: `<svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='32'
+                    height='32'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke='currentColor'
+                >
+                    <path
+                        stroke-linecap='round'
+                        stroke-linejoin='round'
+                        stroke-width='2'
+                        d='M4 4v16l8-8m-8 8l8-8'
+                    ></path>
+                </svg>`,
+    },
+];
+
+export default function CompanyIcons() {
+    return <></>;
+}
