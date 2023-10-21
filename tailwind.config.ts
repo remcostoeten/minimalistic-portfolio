d09f9ca2c23451b68cc088cb245e59c9548a909f
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-21 18:33:17 +0200

    modify tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,52 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
-  },
-  plugins: [],
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      }, 
+      padding: {
+        'card-outer': '16px',
+        'card-inner': '14px',
+        default: '24px',
+      },
+      gap: {
+        xs: '8px',
+        s: '12px',
+        m: '16px',
+        l: '24px',
+      },
+      backgroundColor: {
+        'black': '#111',
+        'grid': '#E4E5E4',
+        'card-inner': '#E4E5E4',
+      },
+    },
+    plugins: [require("tailwindcss-animate")],
+  }
 }
-export default config
