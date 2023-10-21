75520cb1fe8b7aaa625fabcda5a91aa0eed59d45
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-21 23:56:22 +0200

    finished work experience block

--- lib/animations.ts
+++ lib/animations.ts
@@ -1,17 +1,72 @@
 import { Variants } from 'framer-motion';
 
 export const containerVariants: Variants = {
-    hidden: { opacity: 0 },
-    visible: {
-        opacity: 1,
-        transition: {
-            delayChildren: 0.5,
-            staggerChildren: 0.2,
-        },
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
     },
+  },
+};
+export const fadeInUp: Variants = {
+  hidden: { opacity: 0, y: 60 },
+  visible: { opacity: 1, y: 0 },
 };
 
 export const itemVariants: Variants = {
-    hidden: { opacity: 0, y: -10 },
-    visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: -10 },
+  visible: { opacity: 1, y: 0 },
+};
+export const bounceVariant = {
+  hidden: { opacity: 0, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      damping: 10,
+      stiffness: 100,
+    },
+  },
+};
+export const fadeInVariant = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+export const slideInLeftVariant = {
+  hidden: { opacity: 0, x: -50 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+export const slideInRightVariant = {
+  hidden: { opacity: 0, x: 50 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+export const scaleVariant = {
+  hidden: { opacity: 0, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
 };
--- lib/experience.ts
+++ lib/experience.ts
@@ -1,28 +1,31 @@
 export const WorkExperience = [
     {
-        
-        title: 'College - graduated',
+        company: 'Pleio',
+        title: 'Front end developer',
         description:
-            'Graduated as graphic designer.',
-        date: '2013 - 2017',
+            'Frontend for non profit and goverment.',
+        date: '2023 - now',
+        highlight: true,
     },
     {
-        title: 'Tickles digital agency',
-        description:
-            'Magento 2 front-end developer.',
-        date: '2017 - 2021',
-    },
-    {
-        title: 'Distil / Lasaulec B.V.',
+        company: 'Distil / Lasaulec B.V.',
+        title: 'Software developer',
         description:
             'Software (FE) for SaaS and inhouse tooling.',
         date: '2021 - 2023',
     },
     {
-        title: 'Pleio',
+        company: 'Tickles Digital Agency',
+        title: 'Magento 2 FE developer',
         description:
-            'Frontend for non profit and goverment.',
-        date: '2023 - current',
-        highlight: true,
+            'Frontend for digital agency.',
+        date: '2017 - 2021',
+        },
+    {
+        company: 'College - graduated',
+        title: 'Graphic designer',
+        description:
+            'Graduated as graphic designer.',
+        date: '2013 - 2017',
     },
-];
+]
