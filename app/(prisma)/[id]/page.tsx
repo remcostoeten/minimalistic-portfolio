17676a636a0fbe366c61c3fcb507f0a07a7a369d
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-24 00:26:05 +0200

    Prisma stuff

---
+++ app/(prisma)/[id]/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { notFound, } from 'next/navigation'
+import prisma from '../../../lib/prisma'
+import PostDetails from '../../../components/PostDetails'
+
+export default async function Post({ params }: { params: { id: string } }) {
+  const id = Number(
+    Array.isArray(params?.id)
+      ? params?.id[0]
+      : params?.id,
+  )
+
+  if (!id) {
+    notFound();
+    return;
+  }
+
+  const post = await prisma.post.findUnique({
+    where: { id },
+    include: { author: true },
+  })
+
+  if (!post) notFound()
+
+  return (
+    <PostDetails {...post} />
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-import Icon from "@/components/icons/icons";
-import ExperienceItems from "@/components/landing/ExperienceItems";
-import Grid from "@/components/landing/Grid";
-import { WorkExperience } from "@/lib/experience";
 import React from "react";
 
 export default function page() {
-
-  return (
-    <>
-
-      <Grid />      
-
-    </>
-  );
+  return <div>page</div>;
 }
-
-
