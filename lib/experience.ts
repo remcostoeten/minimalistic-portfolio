73be58aa7b975ae45144c860c8e10412e9c62b76
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-31 23:49:57 +0100

    update experience

--- lib/experience.ts
+++ lib/experience.ts
@@ -42,22 +42,30 @@ export const GridItemsData = [
 ]
 
 export const TechnologyIcons = [
-  { name: 'Figma', icon: 'figma.svg', description: 'Design collab', tooltip: 'Web-based design tool', url: 'https://www.figma.com/' },
-  { name: 'GraphQL', icon: 'graphql.svg', description: 'API query', tooltip: 'Backend API query language', url: 'https://graphql.org/' },
-  { name: 'Next', icon: 'next.svg', description: 'React framework', tooltip: 'Server-side React', url: 'https://nextjs.org/' },
+  { name: 'Figma', icon: 'figma.svg', description: 'I miss photoshop', tooltip: 'Actually i dont', url: 'https://www.figma.com/' },
+  { name: 'GraphQL', icon: 'graphql.svg', description: 'API query', tooltip: 'Learnng @ work', url: 'https://graphql.org/' },
+  { name: 'Next', icon: 'next.svg', description: 'Should try Remix..', tooltip: 'Vendor lock-in!', url: 'https://nextjs.org/' },
   { name: 'Turbo', icon: 'turbo.svg', description: 'Fast as fuu booooi', tooltip: 'High-performance build system', url: 'https://turbo.hotwire.dev/' },
   { name: 'Docker', icon: 'docker.svg', description: 'Containerization', tooltip: 'App container platform', url: 'https://www.docker.com/' },
-  { name: 'Firebase', icon: 'firebase.svg', description: 'Dev platform', tooltip: 'Web & mobile dev', url: 'https://firebase.google.com/' },
-  { name: 'HTML', icon: 'html.svg', description: 'Markup lang', tooltip: 'Web markup language', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+  { name: 'Firebase', icon: 'firebase.svg', description: 'Fast prototyping db', tooltip: 'Very fast, goole tho', url: 'https://firebase.google.com/' },
   { name: 'Photoshop', icon: 'photoshop.svg', description: 'Graphic design', tooltip: 'Adobe graphics editor', url: 'https://www.adobe.com/products/photoshop.html' },
   { name: 'Vercel', icon: 'vercel.svg', description: 'Web hosting', tooltip: 'Cloud web hosting', url: 'https://vercel.com/' },
   { name: 'ES6', icon: 'es6.svg', description: 'JS update', tooltip: 'JS language update', url: 'https://www.ecma-international.org/ecma-262/6.0/' },
   { name: 'GitLab', icon: 'gitlab.svg', description: 'DevOps platform', tooltip: 'Git repository manager', url: 'https://about.gitlab.com/' },
   { name: 'Tailwind', icon: 'tailwind.svg', description: 'CSS framework', tooltip: 'Utility-first CSS', url: 'https://tailwindcss.com/' },
-  { name: 'Vim', icon: 'vim.svg', description: 'Text editor', tooltip: 'Highly configurable editor', url: 'https://www.vim.org/' },
-  { name: 'Git', icon: 'git.svg', description: 'Version control', tooltip: 'Source code tracking', url: 'https://git-scm.com/' },
-  { name: 'JavaScript', icon: 'js.svg', description: 'Web scripting', tooltip: 'Interactive web effects', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-  { name: 'React', icon: 'react.svg', description: 'UI library', tooltip: 'JS UI library', url: 'https://reactjs.org/' },
+  { name: 'Neovim', icon: 'vim.svg', description: 'Gigachad status', tooltip: 'Not productive yet...', url: 'https://www.neovim.org/' },
+  { name: 'Git', icon: 'git.svg', description: 'git push -f', tooltip: 'wel..', url: 'https://git-scm.com/' },
+  { name: 'JavaScript', icon: 'js.svg', description: '$(".the").mother(language)', tooltip: 'I miss jQuery', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+  { name: 'React', icon: 'react.svg', description: 'UI library', tooltip: 'JS UI library', url: 'https://reactjs.org/' }, { name: 'Prisma', icon: 'prisma.svg', description: 'Database toolkit', tooltip: 'Next-generation Node.js and TypeScript ORM', url: 'https://www.prisma.io/' },
+
   { name: 'TypeScript', icon: 'ts.svg', description: 'Typed JS', tooltip: 'JS with static typing', url: 'https://www.typescriptlang.org/' },
+  { name: 'Drizzle', icon: 'drizzle.svg', description: 'TyPESaFE ORM', tooltip: 'Complicated AF', url: 'https://www.trufflesuite.com/drizzle' },
+  { name: 'PlanetScale', icon: 'planetscale.svg', description: 'Database platform', tooltip: 'Database platform built on Vitess', url: 'https://planetscale.com/' },
   { name: 'VSCode', icon: 'vsc.svg', description: 'Code editor', tooltip: 'Microsoft code editor', url: 'https://code.visualstudio.com/' },
-];
+  {
+    name: 'Linux',
+    icon: 'linux.svg',
+    description: 'OSX feels so sluggish',
+    tooltip: 'PopOS is the best, never going back.',
+    url: 'https://pop.system76.com/'
+  },];
