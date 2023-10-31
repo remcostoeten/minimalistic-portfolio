f4b8fe9a5bae0a05b8d14cfd20f05e9c23d7f5e2
Remco Stoeten <remcostoeten@hotmail.com>
2023-10-31 23:58:43 +0100

    add titles.

---
+++ components/core/Typography.tsx
@@ -0,0 +1,14 @@
+import { ChildProps } from '@/lib/types/types';
+
+export const Titles = [
+    ({ children }: ChildProps) => (
+        <h1 className="text-4xl font-medium text-gray-100 block box-border leading-6 m-0 p-0 text-left no-underline break-words antialiased ">
+            {children}
+        </h1>
+    ),
+    ({ children }: ChildProps) => (
+        <h2 className="text-lg font-light text-gray-100 block box-border leading-6 m-0 p-0 text-left no-underline break-words antialiased ">
+            {children}
+        </h2>
+    ),
+];
---
+++ components/landing/nav/Intro.tsx
@@ -0,0 +1,3 @@
+import { Children } from "react"
+
+
