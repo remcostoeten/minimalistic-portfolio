68d1f4dd6674c912a6a56611af4300b22a801e45
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-03 20:43:26 +0100

    work on button

--- components/core/buttons.tsx
+++ components/core/buttons.tsx
@@ -13,20 +13,19 @@ const Cta = ({ children }: ChildProps) => {
     )
 }
 
-
 const GhostButton = ({ children }: ChildProps) => {
     return (
         <motion.button
-            className="ghostbtn geist font-normal border-solid bg-body-dark rounded-lg flex flex-row items-center gap-3 text-sm w-auto h-10 px-6 relative box-border antialiased p-4 "
+            className="ghostbtn geist font-normal border-solid bg-body-dark rounded-lg flex flex-row items-center gap-3 text-sm w-auto h-10 px-6 relative box-border antialiased p-2"
             initial={{ opacity: 0, height: 0 }}
             animate={{ opacity: 1, height: "100%" }}
-            transition={{ delay: 0.6, duration: 1, type: "spring", stiffness: 25 }}
+            transition={{ delay: 0.8, duration: 1, type: "spring", stiffness: 25 }}
         >
             <div
                 className="outline-none flex flex-col justify-start flex-shrink-0 text-black transform-none opacity-100 relative flex-none h-auto overflow-visible whitespace-pre w-auto box-border antialiased"
             >
                 <p
-                    className="text-white font-semibold text-lg leading-6 text-start m-0 p-0 box-border antialiased hover:text-gray-300"
+                    className="text-white font-semibold leading-6 text-start m-0 p-0 box-border antialiased hover:text-gray-300"
                 >
                     {children}
                 </p>
