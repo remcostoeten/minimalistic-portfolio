8a6ddc694cb50a41dad1b0673b6eeea6631a9332
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-03 14:56:14 +0100

    F ix merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import { Libre_Baskerville } from 'next/font/google';
 import '../styles/styles.scss';
 import ThemeContextProvider from '@/lib/context/ThemeContext';
-<<<<<<< HEAD
-import NavBar from '@/components/core/Navbar';
-import ActiveSectionContextProvider, { ActiveSectionContext } from '@/lib/context/ActiveSectionContext';
-import { siteConfig } from '@/config/data';
-=======
 import ActiveSectionContextProvider from '@/lib/context/ActiveSectionContext';
 import { siteConfig } from '@/config/site';
 import { GeistMono, GeistSans } from "geist/font";
 import Intro from '@/components/landing/nav/Intro';
 import { Analytics } from '@vercel/analytics/react';
->>>>>>> main
 
 const serif = Libre_Baskerville({
   subsets: ['latin'],
@@ -28,31 +22,6 @@ export const metadata = {
   url: siteConfig.url,
   type: 'Portfolio site and SaaS',
 
-<<<<<<< HEAD
-export const metadata = {
-  title: {
-      default: siteConfig.name,
-      template: `%s | ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  author: 'Remco Stoeten',
-  url: siteConfig.url,
-  type: 'Portfolio site and SaaS',
-
-  openGraph: {
-      type: 'website',
-      url: siteConfig.url,
-      title: siteConfig.name,
-      description: siteConfig.description,
-      site_name: siteConfig.name,
-      image: {
-          url: `${siteConfig.url}/images/og-image.jpg`,
-          alt: "Your Site's Open Graph Image",
-      },
-      profile: {
-          username: 'remco-stoeten',
-      },
-=======
   openGraph: {
     type: 'website',
     url: siteConfig.url,
@@ -66,7 +35,6 @@ export const metadata = {
     profile: {
       username: 'remco-stoeten',
     },
->>>>>>> main
   },
 
   linkedinProfile: 'https://www.linkedin.com/in/remco-stoeten/',
@@ -74,29 +42,6 @@ export const metadata = {
   gitlabProfile: 'https://gitlab.com/remcostoeten',
 
   icons: {
-<<<<<<< HEAD
-      icon: '/favicon.ico',
-      shortcut: '/favicon-16x16.png',
-      apple: '/apple-touch-icon.png',
-  },
-  robots: {
-      index: true,
-
-      follow: true,
-      googleBot: {
-          'index': true,
-          'follow': true,
-          'max-video-preview': -1,
-          'max-image-preview': 'large',
-          'max-snippet': -1,
-      },
-  },
-  authors: [
-      {
-          name: 'Remco Stoeten',
-          url: 'https://remcostoeten.com',
-      },
-=======
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png',
@@ -118,10 +63,8 @@ export const metadata = {
       name: 'Remco Stoeten',
       url: 'https://remcostoeten.com',
     },
->>>>>>> main
   ],
 };
-
 
 export default function RootLayout({
   children,
