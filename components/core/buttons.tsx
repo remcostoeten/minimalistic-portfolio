758cd785dbc73814169a8b015eb8c84acd252ffb
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-03 21:11:36 +0100

    some ui work

--- components/core/buttons.tsx
+++ components/core/buttons.tsx
@@ -13,10 +13,19 @@ const Cta = ({ children }: ChildProps) => {
     )
 }
 
+const CtaGhost = ({ children }: ChildProps) => {
+    return (
+        <button className="cssbuttons-io">
+            <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"></path></svg> Code</span>
+        </button>
+
+    )
+}
+
 const GhostButton = ({ children }: ChildProps) => {
     return (
         <motion.button
-            className="ghostbtn geist font-normal border-solid bg-body-dark rounded-lg flex flex-row items-center gap-3 text-sm w-auto h-10 px-6 relative box-border antialiased p-2"
+            className="ghostbtn geist font-normal border-solid bg-body-dark rounded-lg flex flex-row items-center gap-3 text-sm w-auto h-10 px-6 relative box-border antialiased p-2q"
             initial={{ opacity: 0, height: 0 }}
             animate={{ opacity: 1, height: "100%" }}
             transition={{ delay: 0.8, duration: 1, type: "spring", stiffness: 25 }}
@@ -108,4 +117,4 @@ const glowButton = ({ children }: ChildProps) => {
     )
 }
 
-export { glowButton, Cta, GhostButton }
+export { glowButton, Cta, GhostButton, CtaGhost }
