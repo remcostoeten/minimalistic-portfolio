b72cc3333cf067c62b1a8264348546c2b5ce77c3
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-07 22:23:05 +0100

    some ui work

--- components/landing/Grid.tsx
+++ components/landing/Grid.tsx
@@ -22,7 +22,6 @@ const GridItem = ({ children }: ChildProps) => (
 export default function Grid() {
     return (
         <><h2 className="text-white text-centet text-3xl pb-4">
-            <h2 className="mt-20 text-28  text-white/60 text-center ">My Stack</h2>
 
             <TextRevealSkew className="text-center" placeholderText={[
                 { type: "heading1", text: "About me" },
@@ -33,51 +32,35 @@ export default function Grid() {
                 <div className="flex flex-col items-stretch max-md:w-full">
                     <div className="flex grow flex-col gap-m">
                         <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 0.5, delay: 0.6 }} className="border body-dark-accent dark:body-dark-accent dark:border-dark gap-s self-stretch w-full justify-center items-center flex flex-col p-3.5 rounded-16">
+                            transition={{ duration: 0.5, delay: 0.6 }} className="body-dark-accent dark:body-dark-accent dark:border-dark gap-s self-stretch w-full justify-center items-center flex flex-col p-3.5 rounded-16">
                             <ExperienceItems />
                         </motion.div>
                         <motion.div
                             initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{
-                                duration: 2.2,
+                                duration: 1.2,
                                 delay: 0.85
                             }}
                             className="self-stretch gap-m flex items-center justify-center">
-                            {GridItemsData.map((data, index) => {
-                                if (index === 0) {
-                                    return (
-                                        <GridItem key={index}>
-                                            <h4 className="text-16 text-color-light">{data.title}</h4>
-                                            <h3 className="text-color w-[332px] text-22 text-2xl font-black">
-                                                <CountingNumber start={20} end={data.count} duration={2} delay={index * .2} useFixed={false} />{data.countSuffix}
-                                            </h3>
-                                        </GridItem>
-                                    )
-                                }
-                            })}
-                            {GridItemsData.map((data, index) => {
-                                if (index === 1) {
-                                    return (
-                                        <GridItem key={index}>
-                                            <h4 className="text-16 text-color-light">{data.title}</h4>
-                                            <h3 className="text-color text-22 text-2xl font-black">
-                                                <CountingNumber start={20} end={data.count} duration={2} delay={index * .2} useFixed={false} />{data.countSuffix}
-                                            </h3>
-                                        </GridItem>
-                                    )
-                                }
-                            })}
+                            {GridItemsData.map((data, index) => (
+                                <GridItem key={index}>
+                                    <h4 className="text-16 text-color-light">{data.title}</h4>
+                                    <h3 className="text-color text-22 text-2xl font-black">
+                                        <CountingNumber start={0} end={data.count} duration={2} delay={index * .2} />{data.countSuffix}
+                                    </h3>
+                                </GridItem>
+                            ))}
                         </motion.div>
                     </div>
                 </div>
                 <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{
-                    duration: 2.2,
+                    duration: 1.2,
                     delay: 0.7
                 }}
                     className="flex flex-col items-stretch w-[67%] max-md:w-full">
                     <div className="flex gap-l grow flex-col w-[741px] max-md:max-w-full">
                         <motion.div
                             initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{
-                                duration: 2.2,
+                                duration: 1.2,
                                 delay: 0.25
                             }}
                             className="self-stretch max-md:max-w-full">
@@ -99,11 +82,13 @@ export default function Grid() {
                         <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{
                             duration: 1.2,
                             delay: 1.2
-                        }} className="infinite-slider p-[32px] flex body-dark-accent infinite-slider  w-full gap-[40px] flex-col rounded-16 overflow-hidden body-dark-accent h-full items-center border border-dark flex items-center justify-center">
+                        }} className="p-[32px] flex body-dark-accent dark:body-dark-accent infinite-slider dark:border-dark  w-full gap-[40px] flex-col rounded-16 overflow-hidden body-dark-accent h-full items-center">
+                            <h2 className="text-28  text-white/60 text-center ">My Stack</h2>
                             <div className="flex flex-col gap-l">
                                 <InfiniteLogoSlider />
                             </div>
                         </motion.div>
+                        <Cta>Learn more</Cta>
                     </div>
                 </motion.div>
             </section></>
