ea592f5e2cf9a492902c050478528c6f1e5c27cd
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-16 21:38:25 +0100

    work on some ui stuff

--- components/tables/BetTable.tsx
+++ components/tables/BetTable.tsx
@@ -1,77 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { Input } from '../ui/input';
-import TableSkeleton from '../loaders/Skeleton';
-import { CardBody } from '@nextui-org/react';
-import { Card } from '../ui/card';
+'use client'
 
-type Row = {
-  round: number;
-  bet: any;
-  totalCost: number;
-};
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Table, TableBody, TableHeader, TableRow } from "@nextui-org/table";
+import { TableCell } from "@nextui-org/react";
 
-const BetTable = ({ startingBet, rounds }) => {
-  const [rows, setRows] = useState<Row[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setIsLoading(true);
-    const newRows: Row[] = [];
-    let totalCost = 0;
-    let bet = startingBet;
+function BettingTable() {
+  const [bet1, setBet1] = useState(0);
+  const [bet2, setBet2] = useState(0);
 
-    for (let i = 0; i < rounds; i++) {
-      totalCost += bet;
-      newRows.push({ round: i + 1, bet, totalCost });
-      bet *= 2;
-    }
+  const handleInputChange1 = (event) => {
+    setBet1(event.target.value);
+  };
 
-    setRows(newRows);
-    setIsLoading(false);
-  }, [startingBet, rounds]);
+  const handleInputChange2 = (event) => {
+    setBet2(event.target.value);
+  };
 
   return (
-    <div className='fade'>
-      {isLoading ? (
-        <TableSkeleton />
-      ) : (
-        <table className="min-w-full divide-y shadow-md text-white">
-          <thead className="bg-gray-700">
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Round</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Bet</th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">Total Cost</th>
-            </tr>
-          </thead>
-          <tbody className="bg-gray-800 divide-y divide-gray-700">
-            {rows.map((row, index) => (
-              <tr key={index}>
-                <td className="px-6 py-4 whitespace-nowrap">{row.round}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{row.bet}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{row.totalCost}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+    <div className="p-6">
+      <form className="mb-4 flex gap-2 text-left items-center">
+        <div className='flex flex-col items-center gap-2'>
+          <label htmlFor="bet1" className="block text-sm font-medium text-gray-700">
+            Enter your first bet
+          </label>
+          <Input
+            type="number"
+            id="bet1"
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            value={bet1}
+            onChange={handleInputChange1}
+          />
+        </div>
+        <div className='flex flex-col items-center gap-2'>
+          <label htmlFor="bet2" className="block text-sm font-medium text-gray-700">
+            Enter your second bet
+          </label>
+          <Input
+            type="number"
+            id="bet2"
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            value={bet2}
+            onChange={handleInputChange2}
+          />
+        </div>
+      </form>
+      <Table className="min-w-full divide-y ">
+        <TableHeader>
+          <TableRow>
+            <TableCell className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Step
+            </TableCell>
+            <TableCell className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Bet Size 1
+            </TableCell>
+            <TableCell className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Bet Size 2
+            </TableCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {[...Array(20)].map((_, i) => (
+            <TableRow key={i}>
+              <TableCell className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{i + 1}</TableCell>
+              <TableCell className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{bet1 * Math.pow(2, i)}</TableCell>
+              <TableCell className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{bet2 * Math.pow(2, i)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div >
   );
-};
+}
 
-const BetCalculationTable = () => {
-  const [bet1, setBet1] = useState(1);
-  const [bet2, setBet2] = useState(1);
-  const [rounds, setRounds] = useState(20);
-
-  return (
-    <Card className="p-10 text-white">
-      <Input type="number" value={bet1} onChange={e => setBet1(e.target.value)} className="bg-gray-800 text-white" />
-      <BetTable startingBet={bet1} rounds={rounds} />
-      <Input type="number" value={bet2} onChange={e => setBet2(e.target.value)} className="bg-gray-800 text-white" />
-      <BetTable startingBet={bet2} rounds={rounds} />
-      <Input type="number" value={rounds} onChange={e => setRounds(e.target.value)} className="bg-gray-800 text-white" />
-    </Card>
-  );
-};
-
-export default BetCalculationTable;
+export default BettingTable;
--- config/data/ProjectData.ts
+++ config/data/ProjectData.ts
@@ -1,22 +1,22 @@
 export const ProjectData = [
     {
         title: 'HTML To React/TypeScript converter',
-        description: 'A tool to convert HTML to JSX/TSX. I tend to do a lot of UI prototyping work and got tired of having to wait for the Cloudflare anti-bot loadings on other converters, so I decided to build my own. \n\nThis tool allows you to convert any HTML (including SVGs!) to either JSX or TSX. It supports NextJS app router with client-side flags. It is also possible to generate a fully functioning component with or without props.',
+        description: 'I hate ads and waiting for cloudflare protection so I build my own HTML to JSX/TSX converter, which also supports instant creation of components with props.',
         url: 'https://remcostoeten.com/html-to-jsx',
-        labels: ['React', 'TypeScript', 'NextJS', 'TailwindCSS', 'Context', "Regex"],
+        labels: ['React', 'TypeScript', 'NextJS', 'Tailwind', 'Context', "Regex"],
         icon: '/icons/react.svg'
     },
     {
-        title: "Visual Studio Code recreated in React",
-        description: "Why you ask? I have the same question... This was one of my probably 50 UI's I've created in the past year which was going to be my portfolio site, but I eventually got sick of it. The landing page is very cool though, in my opinion, so it's worth showcasing.\n\n There is one tool in there which is a tool that allows extracting URLs based on certain strings. Again, a tool which I use and I'd rather host myself. But like all, it still has some bugs and is not perfect. But it works... somewhat.", url: 'https://vsc.remcostoeten.com',
+        title: "Visual Studio Code UI recreated in React",
+        description: "Why you ask? I have the same question...Iteration no.lost count for a portfolio idea. Far from done and abandoned, but I do think the landing UI is cool and showworthy nonetheless.", url: 'https://vsc.remcostoeten.com',
         labels: ['TypeScript', 'NextJS', 'TailwindCSS', "SCSS"],
         icon: '/icons/vscodeicon.svg'
     },
     {
         title: "Server side kanban board",
-        description: "This is a kanban board which is server side rendered. It is build with NextJS, TypeScript, Firebase as the database. This was one of my first projects ever created (without any tutorial!). It has authentication which then redirectsd to a kanban board with multiple lanes like we know from *excuses my language* Jira. Also supports drag and drop off the tasks inbetween lanes and such. Loads of features to add but i've called it a day for that project.",
+        description: "My first (post jquery) react creation (without tutorial 😉). Basic kanban board authenticated with Firebase and same goes for the NoSQL database. Create tasks, edit, complete and allows for drag and drop between lanes.",
         url: 'https://kanban.remcostoeten.com',
-        labels: ['DnD', 'TypeScript', 'NextJS', 'TailwindCSS', 'Authentication', "Firebase"],
+        labels: ['DnD', 'TypeScript', 'NextJS', 'TailwindCSS', "Firebase"],
         icon: '/icons/kanban.svg'
     }
 ];
