d5a6785982c0416d29044b4f8655c1c0ce53b1f9
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-19 14:41:47 +0100

    add highlighted blog.

--- components/landing/LatestBlogSingle.tsx
+++ components/landing/LatestBlogSingle.tsx
@@ -12,7 +12,8 @@ export default function LatestBlogSingle() {
                 <SectionTitle>Some articles</SectionTitle>
                 <div className='blog-preview'>
                     {blogPosts.map((post, index) => (
-                        <div key={index} className="w-1/3 flex p-[14px] gap-[24px] flex-col blog-card bg-[#151515] rounded-lg overflow-hidden shadow-lg">
+                        <div key={index}
+                            className={`w-1/3 flex p-[14px] gap-[24px] flex-col blog-card bg-[#151515] rounded-lg overflow-hidden shadow-lg ${post.highlighted ? 'highlighted' : ''}`}>
                             <div className="geist flex justify-between items-center text-sm">
                                 <ShowcaseLabel>{post.label}</ShowcaseLabel>
                                 <div className="flex items-center text-gray-400">
@@ -30,33 +31,31 @@ export default function LatestBlogSingle() {
                                 }}
                                 width="500"
                             />
-                            <div>
-                                <div className="flex justify-between items-start">
-                                    <div>
-                                        <h3 className="font-bold text-xl mb-2 line-clamp-2 text-white text-[24px]">{post.title}</h3>
-                                        <p className="text-[#a3a3a3] line-clamp-2 geist text-16">
-                                            {post.description}
-                                        </p>
-                                    </div>
-                                    <div className="ml-4">
-                                        <a href="#">
-                                            <svg
-                                                className=" h-6 w-6 text-white"
-                                                fill="none"
-                                                height="24"
-                                                stroke="currentColor"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                viewBox="0 0 24 24"
-                                                width="24"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <path d="M7 7h10v10" />
-                                                <path d="M7 17 17 7" />
-                                            </svg>
-                                        </a>
-                                    </div>
+                            <div className="flex justify-between items-start py-2 px-2">
+                                <div>
+                                    <h3 className="font-bold text-xl mb-2 line-clamp-2 text-white text-[24px]">{post.title}</h3>
+                                    <p className="text-[#a3a3a3] line-clamp-2 geist text-16">
+                                        {post.description}
+                                    </p>
+                                </div>
+                                <div className="ml-4">
+                                    <a href="#">
+                                        <svg
+                                            className=" h-6 w-6 text-white"
+                                            fill="none"
+                                            height="24"
+                                            stroke="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            viewBox="0 0 24 24"
+                                            width="24"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path d="M7 7h10v10" />
+                                            <path d="M7 17 17 7" />
+                                        </svg>
+                                    </a>
                                 </div>
                             </div>
                         </div>
--- lib/data/BlogPosts.ts
+++ lib/data/BlogPosts.ts
@@ -4,21 +4,27 @@ export const blogPosts = [
         readTime: "5 min read",
         imageSrc: "/b1.webp",
         title: "Using Firebase in NextJS",
-        description: "A article to showcase how to fetch and post data to Firebase in NextJS app router."
+        description: "A article to showcase how to fetch and post data to Firebase in NextJS app router.",
+        highlighted: true,
+        url: "https://snippets.remcostoeten.com/Components-nextjs-react/firebase-create-firestore-data"
     },
     {
         label: "Framer motion",
         readTime: "2 min read",
-        imageSrc: "/b2.webp",
+        imageSrc: "/b2.png",
         title: "Scroll in view hook",
-        description: "A article to showcase how to use scroll in view hook in framer motion."
+        description: "A article to showcase how to use scroll in view hook in framer motion.",
+        highlighted: false,
+        url: "https://snippets.remcostoeten.com/Components-nextjs-react/scroll-in-view-trigger"
     },
     {
-        label: "",
+        label: "Prisma, PostgreSQL",
         readTime: "2 min read",
-        imageSrc: "/b2.webp",
-        title: "Scroll in view hook",
-        description: "A article to showcase how to use scroll in view hook in framer motion."
+        imageSrc: "/b3.webp",
+        title: "Use Prisma As ORM",
+        description: "A article to showcase how to use Prisma as ORM to post and get data from PostgreSQL.",
+        url: "https://snippets.remcostoeten.com/Components-nextjs-react/prisma",
+        highlighted: false
     }
 ]
 
