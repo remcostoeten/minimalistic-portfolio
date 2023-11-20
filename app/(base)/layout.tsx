a257c9739f1ee5c11f71da204cdb3b1bb15fedf2
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-20 20:35:09 +0100

    wrap app in graphql provider.

--- app/(base)/layout.tsx
+++ app/(base)/layout.tsx
@@ -10,6 +10,8 @@ import Intro from '@/components/landing/nav/Intro';
 import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from 'sonner';
 import RippedNav from '@/components/misc/RippedNav';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from '@/lib/apolloClient';
 
 const serif = Libre_Baskerville({
   subsets: ['latin'],
@@ -74,23 +76,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ThemeContextProvider>
-      <ActiveSectionContextProvider>
-        <html className={`${GeistSans.variable} ${GeistMono.variable} dark text-foreground bg-background`} lang="en">
-          <body className={`${serif.className} dark-background pb-20 min-h-screen flex `}>
-            <span className='absolute top-0 right-0 bg-gradient-to-r from-green-400 to-[##0E0E0E]'></span>
-            <main className="mx-auto pt-8 px-6">
-              <div className="contained">
-                <RippedNav />
-                {children}
-              </div>
-            </main>
-            <Toaster />
-            <Analytics />
-          </body>
-        </html>
-
-      </ActiveSectionContextProvider >
-    </ThemeContextProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeContextProvider>
+        <ActiveSectionContextProvider>
+          <html className={`${GeistSans.variable} ${GeistMono.variable} dark text-foreground bg-background`} lang="en">
+            <body className={`${serif.className} dark-background pb-20 min-h-screen flex `}>
+              <span className='absolute top-0 right-0 bg-gradient-to-r from-green-400 to-[##0E0E0E]'></span>
+              <main className="mx-auto pt-8 px-6">
+                <div className="contained">
+                  <RippedNav />
+                  {children}
+                </div>
+              </main>
+              <Toaster />
+              <Analytics />
+            </body>
+          </html>
+        </ActiveSectionContextProvider >
+      </ThemeContextProvider>
+    </ApolloProvider>
   );
 }
