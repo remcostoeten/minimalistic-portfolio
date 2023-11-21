92822f2b690d38a01c13e7393ea7e3d06246c0a4
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-21 22:40:45 +0100

    minor bugs

--- components/misc/RippedNav.tsx
+++ components/misc/RippedNav.tsx
@@ -8,6 +8,7 @@ import Dropdown from '../core/Dropdown';
 import Logo from '../core/Logo';
 import { SubTitle } from '../core/Typography';
 import SwappingWords from '../effects/SwappingWords';
+import Contact from '../landing/Contact';
 
 export default function RippedNav() {
     const whatsappUrl = `https://wa.me/${phoneNumber}`;
@@ -207,41 +208,7 @@ export default function RippedNav() {
                         <div className=" style-66" data-framer-name="Buttons">
                             <div className="style-67 ">
                                 <div className="style-68 " tabIndex={0}>
-                                    <a
-                                        className="style-69 text-black !bg-[#E5E5E5]"
-                                        data-framer-name="Small"
-                                        href="https://cal.com"
-                                        target="_blank"
-                                        rel="noopener"
-                                    >
-                                        <div
-                                            className="style-70"
-                                            data-framer-component-type="RichTextContainer"
-                                        >
-                                            <p className="">Let's Talk</p>
-                                        </div>
-                                        <div className="style-72">
-                                            <div className="style-73">
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 256 256"
-                                                    focusable="false"
-                                                    color="var(--token-a5634a3e-d7fd-42f9-9a88-14274d3a57cf, rgb(245, 245, 245))"
-                                                    className="style-74"
-                                                >
-                                                    <g
-                                                        color="var(--token-a5634a3e-d7fd-42f9-9a88-14274d3a57cf, rgb(245, 245, 245))"
-                                                        className="style-75"
-                                                    >
-                                                        <path
-                                                            d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"
-                                                            className="style-76"
-                                                        />
-                                                    </g>
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    </a>
+                                    <Contact />
                                 </div>
                             </div>
                         </div>
