106769742f72ae24a93faba73fcdc4ceb359482d
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-21 14:12:51 +0100

    rfm schema

--- lib/graphql/ApolloClient.ts
+++ lib/graphql/ApolloClient.ts
@@ -1,15 +1,3 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  cache: new InMemoryCache(),
-  headers: {
-    Authorization: `Bearer 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`,
-  },
-});
-
-export default client;
-
 // src/apolloClient.ts
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
@@ -61159,5 +61147,5 @@ workflows: [WorkflowFileReference!]!
 
 """
   A valid x509 certificate string
-  """
-  scalar X509Certifica
+"""
+  scalar X509Certificate
