643927badaad0b5085e7a47077b1256d8b97aa91
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-21 23:35:29 +0100

    just some dev work.

--- app/(base)/layout.tsx
+++ app/(base)/layout.tsx
@@ -29,10 +29,10 @@ export default function RootLayout({
 
   children,
 }: {
-
   children: React.ReactNode;
 }) {
 
+<<<<<<< HEAD
   if (__DEV__) {  // Adds messages only in a dev environment
     loadDevMessages();
     loadErrorMessages();
@@ -63,5 +63,26 @@ export default function RootLayout({
         </ActiveSectionContextProvider >
       </ThemeContextProvider>
     </ApolloProvider>
+=======
+return (
+<ThemeContextProvider>
+      <ActiveSectionContextProvider>
+        <html className='dark text-foreground bg-background' lang="en">
+          <body className=' dark-background pb-20 min-h-screen flex'>
+            <span className='absolute top-0 right-0 bg-gradient-to-r from-green-400 to-[##0E0E0E]'></span>
+            <main className="mx-auto pt-8 px-6">
+              <div className="contained">
+                <RippedNav />
+                {children}
+              </div>
+            </main>
+            <Toaster />
+            <Analytics />
+          </body>
+        </html>
+
+      </ActiveSectionContextProvider >
+    </ThemeContextProvider >
+>>>>>>> 6bacca5 ( just some dev work.)
   );
 }
