ffbe58fbf271b6c316ff1a49f98fd234a7f45154
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-21 19:53:47 +0100

    work on infinite sliderf

--- app/(base)/(misc)/transactions/page.tsx
+++ app/(base)/(misc)/transactions/page.tsx
@@ -20,6 +20,7 @@ import TransactionForm from "@/components/transactions/TransactionForm";
 import TransactionList from "@/components/transactions/TransactionList";
 import { toast } from 'sonner';
 import { db } from "@/lib/firebase";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
 
 const TransactionPage = () => {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -142,12 +143,14 @@ const TransactionPage = () => {
     ), [totalDeposits, totalWithdrawals]);
 
     return (
-        <div>
-            <h1>Transaction Tracker</h1>
-            {memoizedTransactionForm}
-            {memoizedTransactionList}
-            {memoizedBalanceDisplay}
-        </div>
+        <Card>
+            <CardHeader><h1>Transaction tracker</h1></CardHeader>
+            <CardBody>
+                {memoizedTransactionForm}
+                {memoizedTransactionList}
+                {memoizedBalanceDisplay}
+            </CardBody>
+        </Card>
     );
 };
 
--- components/effects/InfiniteLogoSlider.tsx
+++ components/effects/InfiniteLogoSlider.tsx
@@ -8,88 +8,56 @@ import Link from 'next/link';
 export default function InfiniteLogoSlider() {
     const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
     const [randomizedIcons, setRandomizedIcons] = useState<{ name: string; icon: string; description: string; tooltip: string; url: string }[] | null>(null);
+    const [randomizedIconsReverse, setRandomizedIconsReverse] = useState<{ name: string; icon: string; description: string; tooltip: string; url: string }[] | null>(null);
 
     useEffect(() => {
-        return setRandomizedIcons([...TechnologyIcons].sort(() => Math.random() - 0.5));
+        setRandomizedIcons([...TechnologyIcons].sort(() => Math.random() - 0.5));
+        setRandomizedIconsReverse([...TechnologyIcons].sort(() => 0.5 - Math.random()));
     }, []);
+
+    const renderSlider = (icons, indexModifier = 0) => (
+        <ul className={`slide-track gap-l ${indexModifier ? 'slider-track-reverse' : ''}`}>
+            {icons.map((icon, index) => (
+                <li
+                    key={index}
+                    onMouseEnter={() => setHoveredIndex(index + indexModifier)}
+                    onMouseLeave={() => setHoveredIndex(null)}
+                >
+                    <Link
+                        className="slide cursor-pointer flex gap gap-[20px] text-20 text-theme dark:bg-[#151515] dark:border-dark dark:text-white/40 bg-card-inner rounded-card-inner relative"
+                        href={icon.url} target="_blank" rel="noopener noreferrer"
+                    >
+                        <Image src={`/icons/${icon.icon}`} alt={icon.name} width={50} height={50} />
+                        <div className="flex flex-col slide__inner">
+                            <span className='text-20 font-extrabold'>{icon.name}</span>
+                            <span className='text-16 text-theme-light'>{icon.description}</span>
+                        </div>
+                        <AnimatePresence>
+                            {hoveredIndex === index + indexModifier && (
+                                <motion.div
+                                    initial={{ opacity: 0, y: -10 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: -10 }}
+                                    className="absolute w-max-content top-[-56px] left-[16px] p-2 bg-card-inner shadow-md text-theme text-sm rounded-card-inner"
+                                >
+                                    {icon.tooltip} <span className="tooltip-triangle"></span>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
 
     return (
         <>
             <div className="slider">
-                <ul className="slide-track gap-l">
-                    {TechnologyIcons.map((icon, index) => (
-                        <motion.li
-                            initial={{ opacity: 0, y: 25 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ delay: 0.3 }}
-                            key={index}
-                            onMouseEnter={() => setHoveredIndex(index)}
-                            onMouseLeave={() => setHoveredIndex(null)}
-                        >
-                            <Link
-                                className="slide cursor-pointer flex gap gap-[20px] text-20 text-theme dark:bg-[#151515] dark:text-white/40  rounded-card-inner relative"
-                                href={icon.url} target="_blank" rel="noopener noreferrer"
-                            >
-                                <Image src={`/icons/${icon.icon}`} alt={icon.name} width={50} height={50} />
-                                <div className="flex flex-col slide__inner">
-                                    <span className='text-20 font-extrabold'>{icon.name}</span>
-                                    <span className='text-16 text-theme-light'>{icon.description}</span>
-                                </div>
-                                <AnimatePresence>
-                                    {hoveredIndex === index && (
-                                        <motion.div
-                                            initial={{ opacity: 0, y: -10 }}
-                                            animate={{ opacity: 1, y: 0 }}
-                                            exit={{ opacity: 0, y: -10 }}
-                                            className="absolute w-max-content top-[-56px] left-[16px] p-2 dark-gradient dark:bg-dark-alt dark:bg-borderdarz dark:text-white/40  text-theme text-sm rounded-card-inner"
-                                        >
-                                            {icon.tooltip} <span className="tooltip-triangle"></span>
-                                        </motion.div>
-                                    )}
-                                </AnimatePresence>
-                            </Link>
-                        </motion.li>
-                    ))}
-                </ul>
+                {randomizedIcons && renderSlider(randomizedIcons)}
             </div>
-
-            {randomizedIcons && (
-                <div className="slider-reverse">
-                    <ul className="slide-track-reverse gap-l">
-                        {randomizedIcons.map((icon, index) => (
-                            <li
-                                key={index}
-                                onMouseEnter={() => setHoveredIndex(index)}
-                                onMouseLeave={() => setHoveredIndex(null)}
-                            >
-                                <Link
-                                    className="slide cursor-pointer flex gap gap-[20px] text-20 text-theme dark:bg-[#151515] dark:border-dark dark:text-white/40 bg-card-inner rounded-card-inner relative"
-                                    href={icon.url} target="_blank" rel="noopener noreferrer"
-                                >
-                                    <Image src={`/icons/${icon.icon}`} alt={icon.name} width={50} height={50} />
-                                    <div className="flex flex-col slide__inner">
-                                        <span className='text-20 font-extrabold'>{icon.name}</span>
-                                        <span className='text-16 text-theme-light'>{icon.description}</span>
-                                    </div>
-                                    <AnimatePresence>
-                                        {hoveredIndex === index && (
-                                            <motion.div
-                                                initial={{ opacity: 0, y: -10 }}
-                                                animate={{ opacity: 1, y: 0 }}
-                                                exit={{ opacity: 0, y: -10 }}
-                                                className="absolute w-max-content top-[-56px] left-[16px] p-2 bg-card-inner shadow-md text-theme text-sm rounded-card-inner"
-                                            >
-                                                {icon.tooltip} <span className="tooltip-triangle"></span>
-                                            </motion.div>
-                                        )}
-                                    </AnimatePresence>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+            <div className="slider-reverse">
+                {randomizedIconsReverse && renderSlider(randomizedIconsReverse, randomizedIcons.length)}
+            </div>
         </>
     );
 }
-
