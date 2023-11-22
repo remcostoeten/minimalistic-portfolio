fb1f969721690c014039cc54a695a19d011225a0
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-22 01:10:48 +0100

    merge apollo client in dev

--- app/(base)/layout.tsx
+++ app/(base)/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Cursor from '@/components/Cursor';
+<<<<<<< HEAD
 import HeaderBar from '@/components/core/HeaderBar';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import client from '@/lib/(graphql)/ApolloClient';
@@ -23,9 +24,16 @@ import { Analytics } from '@vercel/analytics/react';
 import { Libre_Baskerville } from 'next/font/google';
 import { Toaster } from 'sonner';
 import { ApolloProvider } from '@apollo/client';
+=======
+>>>>>>> da3ba22 ( merge apollo client in dev)
 import HeaderBar from '@/components/core/HeaderBar';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import client from '@/lib/(graphql)/ApolloClient';
-import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import ActiveSectionContextProvider from '@/lib/context/ActiveSectionContext';
+import ThemeContextProvider from '@/lib/context/ThemeContext';
+import '@/styles/styles.scss';
+import { ApolloProvider } from '@apollo/client';
+import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
 import { __DEV__ } from '@apollo/client/utilities/globals';
 import { ApolloProvider } from '@apollo/client';
 import HeaderBar from '@/components/core/HeaderBar';
@@ -39,8 +47,6 @@ const serif = Libre_Baskerville({
 });
 
 
-import Cursor from '@/components/Cursor';
-import { TooltipProvider } from '@/components/ui/tooltip';
 export default function RootLayout({
 
   children,
@@ -50,6 +56,7 @@ export default function RootLayout({
 <<<<<<< HEAD
 <<<<<<< HEAD
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 
@@ -67,23 +74,33 @@ export default function RootLayout({
 
 =======
 >>>>>>> 205f6c5 ( add some spinners components/ls)
+=======
+  if (__DEV__) {
+    loadDevMessages();
+    loadErrorMessages();
+  }
+
+>>>>>>> da3ba22 ( merge apollo client in dev)
   return (
     <ApolloProvider client={client}>
       <ThemeContextProvider>
         <ActiveSectionContextProvider>
-          <html className="dark text-foreground bg-background" lang="en">
-            <body className="dark-background pb-20 min-h-screen flex">
-              <span className='absolute top-0 right-0 bg-gradient-to-r from-green-400 to-[##0E0E0E]'></span>
-              <main className="mx-auto pt-8 px-6">
-                <div className="contained">
-                  <HeaderBar />
-                  {children}
-                </div>
-              </main>
-              <Toaster />
-              <Analytics />
-            </body>
-          </html>
+          <TooltipProvider>
+            <html className="dark text-foreground bg-background" lang="en">
+              <body className="dark-background pb-20 min-h-screen flex">
+                <Cursor />
+                <span className='absolute top-0 right-0 bg-gradient-to-r from-green-400 to-[##0E0E0E]'></span>
+                <main className="mx-auto pt-8 px-6">
+                  <div className="contained">
+                    <HeaderBar />
+                    {children}
+                  </div>
+                </main>
+                <Toaster />
+                <Analytics />
+              </body>
+            </html>
+          </TooltipProvider>
         </ActiveSectionContextProvider >
       </ThemeContextProvider>
     </ApolloProvider>
