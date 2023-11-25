f248a52821e4f8487d8f09afafa1b5a2b6f0794c
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-25 21:07:33 +0100

    Update old anchor

--- config/data/ProjectData.ts
+++ config/data/ProjectData.ts
@@ -2,7 +2,7 @@ export const ProjectData = [
     {
         title: 'HTML To React/TypeScript converter',
         description: 'I hate ads and waiting for cloudflare protection so I build my own HTML to JSX/TSX converter, which also supports instant creation of components with props.',
-        url: 'https://remcostoeten.com/html-to-jsx',
+        url: 'https://features.remcostoeten.com/html-to-jsx',
         labels: ['TypeScript', 'Tailwind', 'Context', "Regex"],
         icon: '/icons/react.svg'
     },
