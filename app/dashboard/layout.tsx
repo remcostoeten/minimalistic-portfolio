ecc194b77a5da66bc20206aa60ffde7ca10971d9
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-26 18:20:34 +0100

    fix nav

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 
 import { Navigation } from "@/core/types/types"
 import DashboardNavigation from "@/components/layout/navbar"
+import DashboardNav from "@/components/layout/DashboardNav"
 
 
 const dashboardLinks: Navigation = {
@@ -36,7 +37,7 @@ export default async function DashboardLayout({
             <DashboardNavigation />
             <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
                 <aside className="hidden w-[200px] flex-col md:flex">
-                    {/* <DashboardNav items={dashboardLinks.data} /> */}
+                    <DashboardNav items={dashboardLinks.data} />
                 </aside>
                 <main className="flex w-full flex-1 flex-col">{children}</main>
             </div>
