d6efea8bed9cef6a7208fbb59dcd40a69dd47457
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-27 20:57:47 +0100

    Add new firebase credential

--- core/(database)/firebase.ts
+++ core/(database)/firebase.ts
@@ -6,14 +6,13 @@ import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCabh0AJrarpPaYX4WGowZeor3YQ9809mw",
-    authDomain: "portfolio---debt.firebaseapp.com",
-    projectId: "portfolio---debt",
-    storageBucket: "portfolio---debt.appspot.com",
-    messagingSenderId: "413639141078",
-    appId: "1:413639141078:web:c2ee1dd283d1ac3b1bcb77"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
@@ -33,7 +32,7 @@ const signInWithProvider = (providerName: 'google' | 'github', router) => {
         .catch((error) => {
             console.error(error);
             toast.warning('something went wrong');
-        });
+        }); 
 };
 
 
@@ -42,7 +41,7 @@ export default signInWithProvider;
 const signOut = () => {
     const router = useRouter();
     try {
-         auth.signOut();
+        auth.signOut();
         toast.success('Signed out successfully');
         router.push('/dashboard');
     } catch (e) {
