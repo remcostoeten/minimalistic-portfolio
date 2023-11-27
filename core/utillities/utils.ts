e4a12a7b6ab91cfcc78a678663e91d12767aaf69
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-27 20:57:47 +0100

    Add new firebase credential

--- core/utillities/utils.ts
+++ core/utillities/utils.ts
@@ -35,3 +35,37 @@ export const getErrorMessage = (error: unknown): string => {
 
   return message;
 };
+
+export function formatDate(input: string | number): string {
+  const date = new Date(input)
+  return Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }).format(date)
+}
+
+
+export function dateRangeParams(searchParams: { from: string; to: string }) {
+  if (
+    !searchParams.from ||
+    isNaN(Date.parse(searchParams.from)) ||
+    !searchParams.to ||
+    isNaN(Date.parse(searchParams.to))
+  ) {
+    const dateRange = {
+      from: new Date(),
+      to: new Date(),
+    }
+    const oneYearAgo = new Date()
+    oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1)
+    dateRange.from = oneYearAgo
+
+    return dateRange
+  }
+
+  return {
+    from: new Date(searchParams.from),
+    to: new Date(searchParams.to),
+  }
+}
