db49760483049a36b82906be9ea2a2b8fbbdb75e
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-28 21:35:50 +0100

    add loading shells

--- core/(graphql)/ApolloClient.ts
+++ core/(graphql)/ApolloClient.ts
@@ -4,7 +4,7 @@ const clienttt: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     uri: 'https://api.github.com/graphql',
     cache: new InMemoryCache(),
     headers: {
-        authorization: "Bearer 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",
+        authorization: "Bearer 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",
     },
 });
 
