bc39ac9c804f929bbd1422751551ff6bfa863544
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-29 09:59:43 +0100

    fix typo

--- core/(graphql)/ApolloClient.ts
+++ core/(graphql)/ApolloClient.ts
@@ -4,7 +4,7 @@ const clienttt: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     uri: 'https://api.github.com/graphql',
     cache: new InMemoryCache(),
     headers: {
-        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",
+        Authorization: "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"
         // Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`
     },
 });
