e01adae28fb25cdda9cd6a5a7b76d1edd315700a
Remco Stoeten <remcostoeten@hotmail.com>
2023-11-30 22:29:45 +0100

    update firebase env

--- core/(database)/firebase.ts
+++ core/(database)/firebase.ts
@@ -6,14 +6,13 @@ import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC6eh7Lqnb-LYKVsuXJuStbMe08xZjxoQs",
-    authDomain: "test-ce067.firebaseapp.com",
-    projectId: "test-ce067",
-    storageBucket: "test-ce067.appspot.com",
-    messagingSenderId: "8187414200",
-    appId: "1:8187414200:web:8af3ba670436512f5c164d"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
