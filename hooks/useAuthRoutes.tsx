4d73534f103681e0b5783923e4822ee81fe497f3
Remco Stoeten <remcostoeten@hotmail.com>
2023-12-01 21:12:03 +0100

    add fb hook

---
+++ hooks/useAuthRoutes.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { auth } from "@/core/(database)/firebase";
+
+export function useAuthRedirect(redirectIfAuthed = false, redirectUrl = '/') {
+    const router = useRouter();
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user && redirectIfAuthed) {
+                router.push(redirectUrl);
+            } else if (!user && !redirectIfAuthed) {
+                router.push(redirectUrl);
+            }
+        });
+
+        return () => unsubscribe();
+    }, [router, redirectIfAuthed, redirectUrl]);
